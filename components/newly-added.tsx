b86396ca9bebcba848caa3d75ed9a0d1b1753cc4
unknown <alan.gabriel.luna@hotmail.com>
2026-01-21 12:29:10 -0300

    yakuza

--- components/newly-added.tsx
+++ components/newly-added.tsx
@@ -253,7 +253,7 @@ const newlyAddedGames = [
     name: "Yakuza 20th Anniversary Bundle Platinum Trophy Service",
     description: "Complete Yakuza collection",
     price: 520,
-    category: "ps5",
+    category: "bundles",
     image: "images/games/272.jpg",
     },
 ]
